e4fff79abef3ba5144911f4c906cd62f54ec396f
kimihito <tatsurotamashiro@gmail.com>
2012-06-16 09:52:55 +0900

    first commit

---
+++ sample.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+import xlwt
+
+if __name__ == "__main__":
+
+  book = xlwt.Workbook()
+  newSheet_1 = book.add_sheet("NewSheet_1")
+
+  newSheet_1.write(0,0,"A1")
+
+  newSheet_1_row_1 = newSheet_1.row(1)
+  newSheet_1_row_1.write(0,"A2")
+  newSheet_1_row_1.write(1,"B2")
+  newSheet_1_row_1.write(2,"C2")
+  newSheet_1_row_1.write(3,"D2")
+  newSheet_1_row_1.write(4,"E2")
+
+  book.save('sample.xls')
