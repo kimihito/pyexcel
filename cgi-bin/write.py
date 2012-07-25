447d75b1db0a5d8ef6fbad6ff50a4bcfdc46d96d
kimihito <tatsurotamashiro@gmail.com>
2012-07-26 01:01:26 +0900

    merge index.html

--- cgi-bin/write.py
+++ cgi-bin/write.py
@@ -9,44 +9,48 @@ print "Content-Type: text/html;charset=UTF-8\n\n"
 print "<html><body>"
 
 form = cgi.FieldStorage()
-form_ok = 0
 if form.has_key("name") and form.has_key("price") and form.has_key("piece"):
-   form_ok = 1
-   name = form["name"].value
-   price = form["price"].value
-   piece = form["piece"].value
-#   name = unicode(form["name"].value,"utf-8")
-#   price = unicode(form["price"].value,"utf-8")
-#   piece = unicode(form["piece"].value,"utf-8")
 
-if form_ok == 1:
-  book = xlwt.Workbook(encoding='utf-8')
-  NewSheet_1 = book.add_sheet("NewSheet_2")
-  newSheet_1_row_1 = NewSheet_1.row(1)
-  newSheet_1_row_1.write(0,name)
-  newSheet_1_row_1.write(1,price)
-  newSheet_1_row_1.write(2,piece)
+    params = [form.getlist(param) for param in ["name","price","piece"]]
 
-#for文で登録した商品全部表示させれるようにする
-  print "<h2>Result</h2><br />" 
-  str1 = u"品名:"
-  print str1.encode('utf-8')
-  print unicode(name,"utf-8").encode('utf-8')
-  print "<br \>"
-  str2 = u"価格:"
-  print str2.encode('utf-8')
-  print unicode(price,"utf-8").encode('utf-8')
-  print "<br \>"
-  str3 = u"個数:"
-  print str3.encode('utf-8')
-  print unicode(piece,"utf-8").encode('utf-8')
-  print "<br \>"
+    items =  zip(*params)
+
+    book = xlwt.workbook(encoding='utf-8')
+    newsheet = book.add_sheet("商品登録")
+
+    for i,item in enumerate(items):
+        row = newsheet.row(i+1)
+        putItem(row, item)
+        printItem(item)
+
 else:
-  print "<h1>ERROR</h1>\n\n"
-  print "someform(s) are empty"
+    printError()
 
 print "</body></html>"
 
-#book.save(str(date.today()) +".xls")
 date = date.today()
 book.save(str(date) + ".csv")
+
+def printItem(item):
+    name,price,piece = item
+    print "<h2>Result</h2><br />" 
+    print u"品名".encode('utf-8')
+    print unicode(name,"utf-8").encode('utf-8')
+    print "<br \>"
+    print u"価格:".encode('utf-8')
+    print unicode(price,"utf-8").encode('utf-8')
+    print "<br \>"
+    print u"個数:".encode('utf-8')
+    print unicode(piece,"utf-8").encode('utf-8')
+    print "<br \>"
+
+def putItem(row,item):
+    name,price,piece = item
+
+    row.write(0,name)
+    row.write(1,price)
+    row.write(2,piece)
+
+def printError():
+    print "<h1>ERROR</h1>\n\n"
+    print "someform(s) are empty"
